7175eb6dd3c9ff53f1113490b695f6705ecb27c8
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2022-12-12 15:56:55 +0400

    finally

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,13 +1,14 @@
-import { useState,useReducer } from "react";
+import { useState, useReducer } from "react";
 import Form from "../Form/Form";
 import HideCompleted from "../HideCompletedComponent/HideCompleted";
 import PopUpOpacity from "../PopUpComponent/PopUpOpacity";
 import TasksComponent from "../Tasks/TasksComponent";
 import './Main.scss';
 
+
 // Reducers
 
-function reducer(state,action) {
+function reducer(state, action) {
     if (action.type === "add") {
         return [
             {
@@ -21,34 +22,11 @@ function reducer(state,action) {
     } else if (action.type === "delete") {
         return state.filter((t) => t.id !== action.payload.id);
     } else if (action.type === "hide-completed") {
-
         return state.map((todo) => {
-            if (!action.payload.hidden) {
-
-                // Yes, I know this piece of code reminds us all of one of the popular IT memes 😅, 
-                // but it works😁, and actually this is how it turned out due to my lack of time)
-
-                // What I was aiming for in this piece of code (roughly)
-
-                // if(){
-                //  return {
-                //      ...todo,
-                //  display: !(display value)
-                //  }
-                // }
-
-                if (todo.isCompleted) {
-                    return {
-                        ...todo,
-                        display: true
-                    }
-                }
-            } else {
-                if (todo.isCompleted) {
-                    return {
-                        ...todo,
-                        display: false
-                    }
+            if (todo.isCompleted) {
+                return {
+                    ...todo,
+                    display: !todo.display
                 }
             }
             return todo;
@@ -71,11 +49,11 @@ export default function Main() {
 
     const storedTodos = JSON.parse(localStorage.getItem("todos"));
 
-    const [todos,dispatch] = useReducer(reducer,storedTodos);
-    const [openPopUp,setOpenPopUp] = useState(false);
-    const [hidden,setHidden] = useState(false);
+    const [todos, dispatch] = useReducer(reducer, storedTodos);
+    const [openPopUp, setOpenPopUp] = useState(false);
+    const [hidden, setHidden] = useState(false);
 
-    localStorage.setItem("todos",JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
 
 
 
